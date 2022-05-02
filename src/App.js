688e27488a49fb780fef9e8ebb5a35e43e621877
May Wu <maychangwu@gmail.com>
2022-05-02 00:21:43 -0400

    main container set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Header } from './components';
+import { Header, MainContainer, CreateContainer } from './components';
+import { Route, Routes} from "react-router-dom"
 
 function App() {
   return (
-    <div className='w-screen h-auto flex flex-col items-center'>
+    <div className='w-screen h-auto flex flex-col items-center bg-primary'>
       <Header />
+      <main className='mt-24 p-8 w-full'>
+          <Routes>
+            <Route path='/*' element={<MainContainer/>} />
+            <Route path='/createItem' element={<CreateContainer/>} />
+          </Routes>
+      </main>
     </div>
   );
 }
---
+++ src/components/CreateContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreateContainer() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CreateContainer
