7a55ff315a677adf8a090d373a20054179257763
May Wu <maychangwu@gmail.com>
2022-06-08 00:36:20 -0400

    home component done

--- src/components/HomeContainer.js
+++ src/components/HomeContainer.js
@@ -1,56 +1,72 @@
-import React from "react";
-import Delivery from "../img/delivery.png";
-import HeroBg from "../img/heroBg.png";
-import I1 from '../img/i1.png'
+import React from 'react';
+import Delivery from '../img/delivery.png';
+import HeroBg from '../img/heroBg.png';
+import { heroData } from '../utils/data';
 
 function homeContainer() {
   return (
-    <section className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full" id="home">
-      <div className="py-2 flex-1 flex flex-col items-start justify-start gap-6">
-        <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
-          <p className="text-base text-orange-500 font-semibold">
+    <section className='grid grid-cols-1 md:grid-cols-2 gap-2 w-full' id='home'>
+      <div className='py-2 flex-1 flex flex-col items-start justify-start gap-6'>
+        <div className='flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full'>
+          <p className='text-base text-orange-500 font-semibold'>
             Bike Delivery
           </p>
-          <div className="w-8 h-8 bg-white rounded-full overflow-hidden drop-shadow-xl">
+          <div className='w-8 h-8 bg-white rounded-full overflow-hidden drop-shadow-xl'>
             <img
               src={Delivery}
-              className="w-full h-full object-contain"
-              alt="delivery"
+              className='w-full h-full object-contain'
+              alt='delivery'
             />
           </div>
         </div>
 
-        <p className="text-[2.5rem] lg:text-[3.8rem] font-bold tracking-wide text-headingColor">
-          The Fastest Delivery in{" "}
-          <span className="text-orange-600 text-[3rem] lg:text-[4.5rem]">
+        <p className='text-[2.5rem] lg:text-[3.8rem] font-bold tracking-wide text-headingColor'>
+          The Fastest Delivery in{' '}
+          <span className='text-orange-600 text-[3rem] lg:text-[4.5rem]'>
             Your City
           </span>
         </p>
-        <p className="text-base text-textColor text-center md:text-left md:w-[80%]">
+        <p className='text-base text-textColor text-center md:text-left md:w-[80%]'>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio
           assumenda laborum reprehenderit cum explicabo consequuntur iusto quam
           facere, facilis numquam corrupti, corporis rerum iure debitis vero
           sunt aspernatur repellendus deleniti.
         </p>
         <button
-          type="button"
-          className="bg-gradient-to-br from-orange-400 to-orange-500
-     w-full md:w-auto px-2 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
+          type='button'
+          className='bg-gradient-to-br from-orange-400 to-orange-500
+     w-full md:w-auto px-2 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100'
         >
           Order Now!
         </button>
       </div>
-      <div className="py-2 flex-1 flex items-center relative">
-        <img src={HeroBg} className="ml-auto h-400 w-full lg:w-auto lg:h-650 " alt="hero-bg" />
+      <div className='py-2 flex-1 flex items-center relative'>
+        <img
+          src={HeroBg}
+          className='ml-auto h-400 w-full lg:w-auto lg:h-650 '
+          alt='hero-bg'
+        />
 
-        <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center ">
-            <div className="w-190 p-2 bg-cardOverlay backdrop-blur-md rounded-3xl p-4 flex items-center justify-center flex-col">
-                <img src={I1} alt="I1" className="w-40 -mt-20 " />
-                <p className="text-lg font-semibold text-TextColor mt-4"> Icecream </p>
-                <p className="text-sm text-lighttextGray font-semibold my-3">Chocolate Vanilla</p>
-                <p className="text-md font-semibold text-headingColor">
-                    <span className="text-sm text-red-600">$</span>5.25</p>
-            </div>
+        <div className='w-full h-full absolute top-8 left-0 flex items-center justify-center py-6 lg:px-32 gap-6 lg:gap-4 flex-wrap'>
+          {heroData &&
+            heroData.map((n) => (
+              <div
+                key={n.id}
+                className='lg:w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex items-center justify-center flex-col drop-shadow-md'
+              >
+                <img src={n.imageSrc} alt='I1' className='w-20 -mt-10 lg:w-40 lg:-mt-20 ' />
+                <p className='text-base lg:text-lg font-semibold text-TextColor mt-2 lg:mt-4'>
+                  {n.name}
+                </p>
+                <p className='text-[12px] lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3'>
+                  {n.desc}
+                </p>
+                <p className='text-sm font-semibold text-headingColor'>
+                  <span className='text-sm text-red-600'>$</span>
+                  {n.price}
+                </p>
+              </div>
+            ))}
         </div>
       </div>
     </section>
---
+++ src/utils/data.js
@@ -0,0 +1,12 @@
+import I1 from '../img/i1.png'
+import F1 from '../img/f1.png'
+import C3 from '../img/c3.png'
+import Fi1 from '../img/fi1.png'
+
+
+export const heroData = [
+    {id: 1, name : "Ice cream", desc: 'Chocolate & Vanilla', price: '5.25', imageSrc: I1},
+    {id: 2, name : "Strawberries", desc: 'Fresh Strawberry', price: '10.25', imageSrc: F1},
+    {id: 3, name : "Chicken Kebab", desc: 'Mixed Kebab Plate', price: '8.25', imageSrc: C3},
+    {id: 4, name : "Fish Kebab", desc: 'Mixed Fished Kebab', price: '5.25', imageSrc:  Fi1}
+  ]
